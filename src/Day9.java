a3d8ca7ee3f25c940819f83416dcbc58b10c1347
Mac10goesBRRRT <42710663+Mac10goesBRRRT@users.noreply.github.com>
2023-12-09 19:40:26 +0100

    Day9part2 done 😊😊😊

--- src/Day9.java
+++ src/Day9.java
@@ -11,28 +11,33 @@ public class Day9 {
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
                 String[] numString = data.split(" ");
-                int[] numInt = new int[numString.length+1];
+                int[] numInt = new int[numString.length+2];
                 for(int i = 0; i < numString.length; i++)
-                    numInt[i] = Integer.parseInt(numString[i]);
-                sum1 += calcDiff(numInt)[numInt.length-1];
+                    numInt[i+1] = Integer.parseInt(numString[i]);
+                int[] result = calcDiff(numInt);
+                sum1 += result[result.length-1];
+                sum2 += result[0];
             }
             myReader = new Scanner(file);
             myReader.close();
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
-        System.out.println(sum1);
+        System.out.println("Part 1: " + sum1 + " Part 2: " + sum2);
     }
     private int[] calcDiff(int[] arr){
         int[] temp = new int[arr.length-1];
-        for(int i = 0 ; i < arr.length-2; i++){
+        for(int i = 1 ; i < arr.length-2; i++){
             temp[i] = arr[i+1] - arr[i];
         }
         if(!isZero(temp))
             calcDiff(temp);
-        else
-            arr[arr.length-1] = arr[arr.length-2] + temp[temp.length-1];
+        else {
+            arr[arr.length - 1] = arr[arr.length - 2] + temp[temp.length - 1];
+            arr[0] = arr[1] - temp[1];
+        }
         arr[arr.length-1] = arr[arr.length-2] + temp[temp.length-1];
+        arr[0] = arr[1] - temp[0];
         return arr;
     }
 
