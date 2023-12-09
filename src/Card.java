1fc3541751930fe2ef2bd1dcd1d3ebe2de43475c
Mac10goesBRRRT <42710663+Mac10goesBRRRT@users.noreply.github.com>
2023-12-09 15:26:00 +0100

    Day7part2 done

--- src/Card.java
+++ src/Card.java
@@ -26,30 +26,50 @@ public class Card implements Comparable<Card>{
         return returnValue;
     }
 
-    private int calculateScore(String value) {
+    public int calculateScore(String value) {
         Map<String, Long> result = Arrays.stream(value.split("")).map(String::toLowerCase).collect(Collectors.groupingBy(s->s, LinkedHashMap::new, Collectors.counting()));
         List<Map.Entry<String, Long>> sortedEntries = result.entrySet()
                 .stream()
                 .sorted((entry1, entry2) -> entry2.getValue().compareTo(entry1.getValue()))
                 .toList();
-        //System.out.println(result);
+        //System.out.print(result);
         int retValue = Math.toIntExact(sortedEntries.get(0).getValue());
-        if(retValue > 3)
-             return retValue + 1;
-        if(retValue==3 && sortedEntries.get(1).getValue()==2)
+        int numJokers = 0;
+        int indexJ = -1;
+        for (int i = 0; i < sortedEntries.size(); i++) {
+            if (sortedEntries.get(i).getKey().equals("j")) {
+                indexJ = i;
+                break;
+            }
+        }
+        if(indexJ > -1)
+            numJokers = Math.toIntExact(sortedEntries.get(indexJ).getValue());
+        //System.out.println(retValue + " " + sortedEntries.get(1).getValue() + " " + numJokers);
+        //TODO: viele Sachen mit 1 nem Joker und nur 1er passenden Karte landen zu weit hinten
+        if (retValue == 5 || (retValue == 3 && numJokers == 2) || numJokers == 4 || (retValue==4 && numJokers==1) || (numJokers == 3 && sortedEntries.get(1).getValue()==2)) //5, cant upgrade
+            return  6;
+        if(retValue == 4 || (retValue == 3 && numJokers == 1) || numJokers == 3 || checkQuad(sortedEntries) ) //4, can be upgraded to 5 with any joker, and we simply add the number of Jokers (1 or none)
+             return 5; //simply add Jokers, does not change anything else but make a //TODO hier passt was noch nicht
+        if((retValue==3 && sortedEntries.get(1).getValue()==2) || (retValue == 2 && numJokers == 1 && sortedEntries.get(1).getValue()==2)) //Full House, if we have a Triple and a Pair. Or we have  2 + 2 (so pair of 2 and 2 jokers
             return 4;
-        else if(retValue==3)
+        else if(retValue==3 || numJokers == 2 || (retValue==2 && numJokers == 1)) //Triple, or 2 Jokers and 1 random card
             return 3;
-        else if(retValue==2 && sortedEntries.get(1).getValue()==2)
+        else if((retValue==2 && sortedEntries.get(1).getValue()==2)) //2 Pairs or 1 Pair and 2 jokers. !!! 1 Pair and 1 joker makes a Triple
             return 2;
-        else if(retValue==2)
+        else if(retValue==2 || numJokers == 1) //1 Pair, or 1 Joker and any other card.
             return 1;
         return 0;
     }
 
+    private boolean checkQuad(List<Map.Entry<String, Long>> sortedEntries){
+        if(sortedEntries.get(0).getValue() == 2 && sortedEntries.get(1).getValue() == 2)
+            return(sortedEntries.get(0).getKey().equals("j") || sortedEntries.get(1).getKey().equals("j"));
+        return false;
+    }
+
     private int stringSort (String s1, String s2){
-        String customOrder = "23456789TJQKA";
-        //String customOrder = "J23456789TQKA";
+        //String customOrder = "23456789TJQKA";
+        String customOrder = "J23456789TQKA";
         int value = 0;
         int i = 0;
         do{
--- src/Day7.java
+++ src/Day7.java
@@ -3,24 +3,29 @@ import java.io.FileNotFoundException;
 import java.util.*;
 
 public class Day7 {
+    // 246591849, 246539389, 246628634 too low
+    //NOT 246688347
     public void solveDay7(){
+        //Card c = new Card("AAJ68",1);
+        //int i = c.calculateScore("AAJ68");
+        //System.out.println(i);
         List<Card> cardList = getInput();
         cardList.sort(null);
         for(Card c : cardList)
-            System.out.println(c.getValue() + " " + c.getPower());
+            //System.out.println(c.getValue() + " " + c.getPower() + " " + (c.calculateScore(c.getValue())));
+            System.out.println(c.getValue());
         int sum1 = 0;
         for(int i = 0 ; i < cardList.size(); i++){
             sum1 += cardList.get(i).getPower() * (i+1);
         }
         System.out.println(sum1);
-
     }
 
     private List<Card> getInput(){
         List<Card> cardList = new ArrayList<>();
         Scanner myReader;
         try {
-            myReader = new Scanner(new File("./input/day7/sampleInput.txt"));
+            myReader = new Scanner(new File("./input/day7/day7.txt"));
             int gameCounter = 1;
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
